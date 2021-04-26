399abf0ec5bccaf01c2f777cdab250259ee307e0
Alécio Santana <aleciosantana@gmail.com>
2021-04-26 15:46:49 -0300

    Middleware: Global middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,11 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { CastModule } from './cats/cats.module';
-import { logger } from './logger.middleware';
 
 @Module({
   imports: [CastModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(logger)
-      .exclude(
-        { path: 'cats', method: RequestMethod.GET },
-        { path: 'cats', method: RequestMethod.POST },
-      )
-      .forRoutes(CatsController);
-  }
-}
+export class AppModule {}
